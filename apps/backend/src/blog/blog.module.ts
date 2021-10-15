0aa4d4ec5c3a0f29d4fb7d17fdd23dfe63939a8e
Aleksander Chromik <aleksander.chromik@footballco.com>
2021-10-16 00:18:19 +0200

    Add authentication middleware

--- apps/backend/src/blog/blog.module.ts
+++ apps/backend/src/blog/blog.module.ts
@@ -1,12 +1,40 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+
 import { BlogService } from './blog.service';
 import { BlogController } from './blog.controller';
 import { BlogSchema } from './schemas/blog.schema';
+import { AuthenticationMiddleware } from '../middlewares/authentication.middleware';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'Post', schema: BlogSchema }])],
+  imports: [
+    ConfigModule,
+    MongooseModule.forFeature([{ name: 'Post', schema: BlogSchema }]),
+  ],
   providers: [BlogService],
   controllers: [BlogController],
 })
-export class BlogModule {}
+export class BlogModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): MiddlewareConsumer | void {
+    consumer.apply(AuthenticationMiddleware).forRoutes(
+      {
+        method: RequestMethod.POST,
+        path: 'blog/post',
+      },
+      {
+        method: RequestMethod.PUT,
+        path: '/blog/edit',
+      },
+      {
+        method: RequestMethod.DELETE,
+        path: '/blog/delete',
+      }
+    );
+  }
+}
