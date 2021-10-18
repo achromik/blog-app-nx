9224e520793b796021ac4fe3c5f88b10c1502eff
Aleksander Chromik <aleksander.chromik@footballco.com>
2021-10-18 17:07:40 +0200

    Cleanup in user controller

--- apps/backend/src/user/user.controller.ts
+++ apps/backend/src/user/user.controller.ts
@@ -1,27 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUserDTO } from './dto/create-user.dto';
+import { Controller } from '@nestjs/common';
 
-import { User } from './interfaces/user.interface';
 import { UserService } from './user.service';
-
-interface UserResponse {
-  message: string;
-  user: User;
-}
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
-
-  @Post('register')
-  async createUser(
-    @Body() createUserDTO: CreateUserDTO
-  ): Promise<UserResponse> {
-    const newUser = await this.userService.create(createUserDTO);
-
-    return {
-      message: 'User has been registered successfully!',
-      user: newUser as User,
-    };
-  }
 }
