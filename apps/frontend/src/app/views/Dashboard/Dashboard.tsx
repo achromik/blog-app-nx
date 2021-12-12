a558600b55d9e5e48e852bd2c0b778509cee68cd
Aleksander Chromik <aleksander.chromik@footballco.com>
2021-12-12 21:24:23 +0100

    Move DashboardLayout to layouts folder

--- apps/frontend/src/app/views/Dashboard/Dashboard.tsx
+++ apps/frontend/src/app/views/Dashboard/Dashboard.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { MainLayout } from '../../components/MainLayout/MainLayout';
+import { DashboardLayout } from '../../layouts/DashboardLayout/DashboardLayout';
 import { ROUTES } from '../../config';
 import { Me } from './views';
 import { Users } from './views/Users/Users';
 
 export const Dashboard: React.FC = () => (
-  <MainLayout title="The dashboard">
+  <DashboardLayout title="The dashboard">
     <Routes>
       <Route path="/" element={<h1>The Dashboard</h1>} />
       <Route path={ROUTES.dashboardRoute.users} element={<Users />} />
       <Route path={ROUTES.dashboardRoute.me} element={<Me />} />
       <Route path="*" element={<div>Not Found</div>} />
     </Routes>
-  </MainLayout>
+  </DashboardLayout>
 );
