6ee44d06b19e94269000b8ecc888c929175cdd3d
Aleksander Chromik <aleksander.chromik@footballco.com>
2021-12-13 22:35:37 +0100

    Add Row to NotFound view

--- apps/frontend/src/app/views/NotFound/NotFound.tsx
+++ apps/frontend/src/app/views/NotFound/NotFound.tsx
@@ -1,10 +1,12 @@
-import { Button, Result } from 'antd';
+import { Button, Result, Row } from 'antd';
 
 export const NotFound: React.FC = () => (
-  <Result
-    status="404"
-    title="404"
-    subTitle="Sorry, the page you visited does not exist."
-    extra={<Button type="primary">Back Home</Button>}
-  />
+  <Row style={{ height: '100vh' }} justify="center" align="middle">
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={<Button type="primary">Back Home</Button>}
+    />
+  </Row>
 );
